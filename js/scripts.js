f25d2eb174e00ac95ab35ef6d104f2ec74c19f73
Liz <adhiamboliz3@gmail.com>
2022-03-18 14:32:36 +0300

    added switch function to pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,20 @@ function switchTopping() {
       }
     }
   }
+
+  function switchClassic() {
+    if (document.getElementById('classic')) {
+    
+        if (document.getElementById('classic').style.display == 'none') {
+            document.getElementById('toppings').style.display = 'none';
+            document.getElementById('classic').style.display = 'block';
+            document.getElementById('deluxe').style.display = 'none';
+        }
+        else {
+            document.getElementById('toppings').style.display = 'none';
+            document.getElementById('classic').style.display = 'block';
+            document.getElementById('deluxe').style.display = 'none';
+        }
+      }
+    }
+    
