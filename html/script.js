1a63c27070fd8ddb5d0643da61f412e7b4c6f6df
Liz <adhiamboliz3@gmail.com>
2022-03-19 21:49:39 +0300

    added fumction to calculate pizza prize

--- html/script.js
+++ html/script.js
@@ -10,3 +10,23 @@ function Pizza(pizza, size, crust, toppings,) {
 
 var priceSize, priceCrust, priceTopping;
 
+//calculation point
+var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
+    switch (pizzaSize) {
+        case "":
+            priceSize = 0;
+            break;
+        case "large":
+            priceSize = 1200;
+            break;
+        case "medium":
+            priceSize = 800;
+            break;
+        case "small":
+            priceSize = 450;
+            break;
+        default:
+            location.reload();
+            alert("Please select a pizza size");
+    };
+  
